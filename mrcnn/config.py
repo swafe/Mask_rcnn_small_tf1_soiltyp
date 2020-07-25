a4a10fe9fff3d996e2f2cadb1ffc680562055801
swafe <svenfachinger@web.de>
2020-07-25 20:05:13 +0200

    tensorboard callbacks to soiltype_callbacks.py

--- mrcnn/config.py
+++ mrcnn/config.py
@@ -172,7 +172,7 @@ class Config(object):
     DETECTION_MIN_CONFIDENCE = 0.7
 
     # Non-maximum suppression threshold for detection
-    DETECTION_NMS_THRESHOLD = 0.3
+    DETECTION_NMS_THRESHOLD = 0.7
 
     # Learning rate and momentum
     # The Mask RCNN paper uses lr=0.02, but on TensorFlow it causes
